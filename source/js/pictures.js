b8120fcb21ce05c3b7fba2f3f1ecc9e534a7e240
Yeah.not <yeahhh.not@gmail.com>
2021-01-04 19:20:53 +0200

    Добавил генерацию данных фотографий

--- source/js/pictures.js
+++ source/js/pictures.js
@@ -1,18 +1,58 @@
 'use strict';
 
 var PICTURES_NUM = 25;
-
-var pictures = [
-  {
-    url: 'photos/1.jpg',
-    likes: 15,
-    comments: [
-      'Всё отлично!',
-      'В целом всё неплохо. Но не всё.'
-    ],
-    description: 'Тестим новую камеру!'
-  }
+var LIKES_NUM = [15, 200];
+var COMMENTS_NUM = [3, 50];
+var COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
 ];
+var DESCRIPTIONS = [
+  'Тестим новую камеру!',
+  'Затусили с друзьями на море',
+  'Как же круто тут кормят',
+  'Отдыхаем...',
+  'Цените каждое мгновенье. Цените тех, кто рядом с вами и отгоняйте все сомненья. Не обижайте всех словами......',
+  'Вот это тачка!',
+];
+
+
+var getRandomEl = function(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+var getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var getComments = function(num) {
+  var comments = [];
+
+  for (var i = 0; i < num; i++) {
+    comments[i] = getRandomInt(0, 1) ?  getRandomEl(COMMENTS) : getRandomEl(COMMENTS) + ' ' + getRandomEl(COMMENTS);
+  }
+
+  return comments;
+}
+
+var getPictures = function(num) {
+  var pictures = [];
+
+  for (var i = 0; i < num; i++) {
+    pictures[i] = {
+      url: 'photos/' + getRandomInt(1, PICTURES_NUM) + '.jpg',
+      likes: getRandomInt(LIKES_NUM[0], LIKES_NUM[1]),
+      comments: getComments(getRandomInt(COMMENTS_NUM[0], COMMENTS_NUM[1])),
+      description: getRandomEl(DESCRIPTIONS),
+    };
+  }
+
+  return pictures;
+}
 
 var renderPicture = function(picture) {
   var pictureElem = pictureTemplate.cloneNode(true);
@@ -27,8 +67,8 @@ var renderPicture = function(picture) {
 var renderPictures = function(pictures) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < PICTURES_NUM; i++) {
-    fragment.appendChild(renderPicture(pictures[0]));
+  for (var i = 0; i < pictures.length; i++) {
+    fragment.appendChild(renderPicture(pictures[i]));
   }
 
   return fragment;
@@ -37,4 +77,5 @@ var renderPictures = function(pictures) {
 var picturesContainer = document.querySelector('.pictures');
 var pictureTemplate = document.querySelector('#picture').content.querySelector('.picture__link');
 
+var pictures = getPictures(PICTURES_NUM);
 picturesContainer.appendChild(renderPictures(pictures));

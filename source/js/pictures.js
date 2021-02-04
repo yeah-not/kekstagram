00bdf885c9bb8ae3fcdfe0482689bce8075f77ca
yeah-not <yeahhh.not@gmail.com>
2021-02-04 11:29:28 +0200

    Валидация формы изображения - полей хэштэгов и комментария

--- source/js/pictures.js
+++ source/js/pictures.js
@@ -196,9 +196,7 @@ var closeUploadPopup = function() {
 };
 
 var onUploadPopupEscPress = function(evt) {
-  if (evt.code === 'Escape'
-    && evt.target !== uploadHashtags
-    && evt.target !== uploadDescription) {
+  if (evt.code === 'Escape') {
     closeUploadPopup();
   }
 };
@@ -366,10 +364,110 @@ sizePlus.addEventListener('click', function() {
 // Валидация формы загрузки изображения
 // --------------
 
+var TAG_MAX_LENGTH = 20;
+var TAGS_MAX_NUM = 5;
+var TAGS_PATTERN = /[^\w\sА-Яа-яЁё\# -]/;
+var COMMENT_MIN_LENGTH = 20;
+var COMMENT_MAX_LENGTH = 140;
+
+var ERROR_NO_HASH = 'Хэш-тег должен начинаться с символа # (решётка)';
+var ERROR_ONLY_HASH = 'Хэш-тег не может состоять только из одной решётки';
+var ERROR_TAG_TOO_LONG = 'Максимальная длина одного хэш-тега ' + TAG_MAX_LENGTH + ' символов, включая решётку';
+var ERROR_TAG_NOT_UNIQUE = 'Один и тот же хэш-тег не может быть использован дважды';
+var ERROR_TAGS_PATTERN = 'Хэш-теги разделяются пробелами. Используются буквы, цифры, символы "-", "_"';
+var ERROR_TAGS_NUM = 'Можно указать не больше ' + TAGS_MAX_NUM + ' хэш-тегов';
+var ERROR_COMMENT_TOO_SHORT = 'Минимальная длина комментария ' + COMMENT_MIN_LENGTH + ' cимволов, хотя он и не обязателен;)';
+var ERROR_COMMENT_TOO_LONG = 'Максимальная длина комментария ' + COMMENT_MAX_LENGTH + ' cимволов, хотя он и не обязателен;)';
+
 // Функции
 // --------------
+var validateHashtag = function(hashtag, index, hashtags) {
+  var firstChar = hashtag[0];
+  var tagCloneIndex = hashtags.indexOf(hashtag, index + 1);
+  var errorMsg = '';
+
+  if (firstChar !== '#') {
+    errorMsg = ERROR_NO_HASH;
+  } else if (firstChar === '#' && hashtag.length === 1) {
+    errorMsg = ERROR_ONLY_HASH;
+  } else if (hashtag.length > TAG_MAX_LENGTH) {
+    errorMsg = ERROR_TAG_TOO_LONG;
+  } else if (tagCloneIndex > 0) {
+    errorMsg = ERROR_TAG_NOT_UNIQUE;
+  } else {
+    errorMsg = '';
+  }
+
+  return errorMsg;
+};
+
+var validateHashtagsInput = function(input) {
+  var tagsStr = input.value;
+  var tags = tagsStr.trim().toLowerCase().split(' ');
+  var errorMsg = '';
+
+  tags = tags.filter(function(el) {
+    return el !== '';
+  });
+
+  if (tagsStr.match(TAGS_PATTERN)) {
+    errorMsg = ERROR_TAGS_PATTERN;
+  } else if (tags.length > TAGS_MAX_NUM) {
+    errorMsg = ERROR_TAGS_NUM;
+  } else {
+    for (var i = 0; i < tags.length; i++) {
+      errorMsg = validateHashtag(tags[i], i, tags);
+      if (errorMsg) {
+        break;
+      }
+    }
+  }
+
+  input.setCustomValidity(errorMsg);
+  input.reportValidity();
+};
+
 // Старт
 // --------------
 var uploadForm = document.querySelector('.img-upload__form');
-var uploadHashtags = uploadForm.querySelector('.text__hashtags');
-var uploadDescription = uploadForm.querySelector('.text__description');
+var uploadText = uploadForm.querySelector('.img-upload__text');
+var hashtagsInput = uploadText.querySelector('.text__hashtags');
+var commentInput = uploadText.querySelector('.text__description');
+
+uploadForm.addEventListener('submit', function(evt) {
+  validateHashtagsInput(hashtagsInput);
+
+  if (!hashtagsInput.validity.valid) {
+    evt.preventDefault();
+  }
+});
+
+commentInput.addEventListener('invalid', function() {
+  if (commentInput.validity.tooShort) {
+    commentInput.setCustomValidity(ERROR_COMMENT_TOO_SHORT);
+  } else if (commentInput.validity.tooLong) {
+    commentInput.setCustomValidity(ERROR_COMMENT_TOO_LONG);
+  } else {
+    commentInput.setCustomValidity('');
+  }
+});
+
+uploadText.addEventListener('invalid', function(evt) {
+  evt.target.classList.add('input-error');
+}, true);
+
+uploadText.addEventListener('input', function(evt) {
+  var target = evt.target;
+
+  target.setCustomValidity('');
+
+  if (evt.target.validity.valid) {
+    evt.target.classList.remove('input-error');
+  }
+});
+
+uploadText.addEventListener('keydown', function(evt) {
+  if (evt.code === 'Escape') {
+    evt.stopPropagation();
+  }
+});

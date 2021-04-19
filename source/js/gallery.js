05e71089db40353178c30814b324790050849413
yeah-not <yeahhh.not@gmail.com>
2021-04-19 11:29:59 +0300

    Перенес обработчик нажатия на превью в gallery. Gallery теперь зависим от picture и preview, которые независимы друг от друга.

--- source/js/gallery.js
+++ source/js/gallery.js
@@ -3,16 +3,43 @@
 // --------------
 //  Галерея изображений
 // --------------
-// Зависимости: util, data, message, backend, picture
+// Зависимости: util, data, message, backend, picture, preview
 
 (function() {
   var ERROR_LOAD = 'Изображения не загружены. ';
 
   var pictures = document.querySelector('.pictures');
 
-  var onLoad = function(data) {
+  var render = function(data) {
     // data = window.data.generatePictures();
-    pictures.appendChild(window.util.renderFragment(data, window.picture.render));
+    // Вариант 1 - IIFE-обход потери окружения
+    var fragment = document.createDocumentFragment();
+
+    for (var i = 0; i < data.length; i++) {
+      var picture = window.picture.render(data[i]);
+
+      (function(pictureElem, pictureData) {
+        pictureElem.addEventListener('click', function() {
+          window.preview.open(pictureData);
+        });
+      })(picture, data[i]);
+
+      fragment.appendChild(picture);
+    }
+
+    // Вариант 2 - обработчик передается как callback
+    // var onPictureClick = function(picture, data) {
+    //   picture.addEventListener('click', function() {
+    //     window.preview.open(data);
+    //   });
+    // }
+    //
+    // var fragment = window.util.renderFragment(data, window.picture.render, onPictureClick)
+    pictures.appendChild(fragment);
+  };
+
+  var onLoad = function(data) {
+    render(data);
   };
 
   var onError = function(error) {

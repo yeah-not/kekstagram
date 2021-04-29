c07b9e055a2f87ea663a8e7c386125581c20617d
yeah-not <yeahhh.not@gmail.com>
2021-04-29 16:26:29 +0300

    Рефакторинг модуля scale и перенос управления в image-effect

--- source/js/main.js
+++ source/js/main.js
@@ -5,23 +5,6 @@
 // Зависимости: остальные модули
 
 (function() {
-  var effectScale = new window.Scale('.img-upload__scale');
-
-  effectScale.onChange = function(level) {
-    window.imageEffect.apply(level);
-  };
-
-  window.imageEffect.onSwitch = function() {
-    effectScale.reset();
-  };
-
-  window.imageEffect.onApply = function(effect) {
-    if (effect === 'none') {
-      effectScale.hide();
-    } else {
-      effectScale.show();
-    }
-  };
 
   window.upload.onOpen = function() {
     window.imageEffect.apply();
@@ -29,7 +12,6 @@
 
   window.upload.onClose = function() {
     window.uploadForm.reset();
-    effectScale.reset();
     window.imageEffect.reset();
     window.imageSize.reset();
   };
--- source/js/scale.js
+++ source/js/scale.js
@@ -5,7 +5,9 @@
 // --------------
 // Зависимости: нет
 
-var Scale = function(selector) {
+var Scale = function(selector, value) {
+  this.value = value || this.Value.DEFAULT;
+
   this.el = document.querySelector(selector || this.DEFAULT_SELECTOR);
   this.elLine = this.el.querySelector('.scale__line');
   this.elPin = this.el.querySelector('.scale__pin');
@@ -22,10 +24,17 @@ Scale.prototype = {
     MAX: 100,
     DEFAULT: 100
   },
-  setValue: function(level) {
-    var value = Math.round(level);
-    this.elValue.value = value;
-    return value;
+  update: function(level, noCallBack) {
+    this._updateLevel(level);
+    this._setValue(level);
+    this.elValue.value = this.value;
+
+    if (!noCallBack) {
+      this.onUpdate(this.value);
+    }
+  },
+  reset: function() {
+    this.update(this.Value.DEFAULT, true);
   },
   show: function() {
     window.util.show(this.el);
@@ -33,11 +42,7 @@ Scale.prototype = {
   hide: function() {
     window.util.hide(this.el);
   },
-  reset: function() {
-    this.setValue(this.Value.DEFAULT);
-    this._updateLevel(this.Value.DEFAULT);
-  },
-  onChange: function(value) {
+  onUpdate: function(value) {
     return value;
   },
   _calcLevel: function(shift) {
@@ -50,6 +55,9 @@ Scale.prototype = {
     this.elPin.style.left = levelCSS;
     this.elLevel.style.width = levelCSS;
   },
+  _setValue: function(level) {
+    this.value = Math.round(level);
+  },
   _onPinMouseDown: function(evt) {
     var self = this;
     var startX = evt.clientX;
@@ -61,8 +69,7 @@ Scale.prototype = {
       var level = self._calcLevel(shift);
 
       if (level >= self.Value.MIN && level <= self.Value.MAX) {
-        self._updateLevel(level);
-        self.onChange(self.setValue(level));
+        self.update(level);
       }
     };
 
